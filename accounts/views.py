bbbc2fb9e748d4bcba207f5ac15c381d9b793a3a
komalbehera <ishabehera@gmail.com>
2021-11-20 02:32:08 +0530

    files for discussion forum

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import login, logout
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from . import forms
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm  
+    success_url = reverse("login")
+    template_name = "accounts/signup.html"
