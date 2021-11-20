86f1c72884510382531d37363f50d492dd1217a4
komalbehera <ishabehera@gmail.com>
2021-11-20 11:55:30 +0530

    creating workspace environment for project

---
+++ new-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dell\desktop\projects\microsoft-engage-project\new-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
